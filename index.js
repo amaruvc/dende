daae06c7449cba816042e9411b15e225bd5574fd
Amaru <amaruvc@gmail.com>
2022-02-08 22:20:19 -0300

    desafio dende

---
+++ index.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const moment = require("moment"); // require
+const chalk = require("chalk");
+
+const app = express();
+
+app.use(express.static("static"));
+
+app.get("/", async (req, res) => {
+  const resp = await axios.get("https://randomuser.me/api/?results=20");
+  const data = resp.data.results;
+
+  const usuarios = data.map((x) => {
+    const usuario = {
+      id: uuidv4(),
+      nombre: x.name.first,
+      apellido: x.name.last,
+      fecha_registro: moment().format("MMMM Do YYYY, h:mm:ss a"),
+    };
+    return usuario;
+  });
+
+  const usuariosFormateados = usuarios.map((x, i) => {
+    return `${i + 1}. Nombre: ${x.nombre} - Apellido: ${x.apellido} - ID: ${
+      x.id
+    } - Timestamp: ${x.fecha_registro}`;
+  });
+  res.send(usuariosFormateados.join("<br />"));
+
+  console.log(chalk.blue.bgWhite(usuariosFormateados.join("\n")));
+});
+
+//Server
+app.listen(5050, () => {
+  console.log("Server up en puerto 5050");
+});
